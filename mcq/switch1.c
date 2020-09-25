b4af395bd7dd5b107fe054d1fc7633eef8555372
Aman Kathait <agnikathait17149@gmail.com>
2020-09-25 20:17:45 +0530

    mcq updated

---
+++ mcq/switch1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int i = 3;
+    switch(i)
+    {
+        printf("Outside ");
+        case 1: printf("Geeks");
+            break;
+        case 2: printf("Quiz");
+            break;
+        defau1t: printf("GeeksQuiz");
+    }
+    return 0;
+}
